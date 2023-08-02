94fd7d5ae6fea2dabf800037cba6a697b87097ba
Salvador Castaneda <57605337+salcasta@users.noreply.github.com>
2023-08-02 22:11:03 +0000

    project complete

--- app.rb
+++ app.rb
@@ -2,8 +2,43 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  require "http"
+  currency_url = "https://api.exchangerate.host/symbols"
+  raw_response = HTTP.get(currency_url)
+
+  require "json"
+  parsed_response = JSON.parse(raw_response)
+  @symbols_hash = parsed_response.fetch("symbols")
+
+  erb(:homepage)
+end
+
+
+get("/:main_curr") do
+  @currency_choice = params.fetch("main_curr")
+
+  require "http"
+  response = HTTP.get('https://api.exchangerate.host/symbols')
+
+  require "json"
+  parsed_response = JSON.parse(response)
+
+  @symbols_hash = parsed_response.fetch('symbols')
+
+  erb(:currency_page)
+end
+
+get("/:main_curr/:secondary_curr") do
+  @currency_one = params.fetch("main_curr")
+  @currency_two = params.fetch("secondary_curr")
+
+  require "http"
+  convert_response = HTTP.get("https://api.exchangerate.host/convert?from=#{@currency_one}&to=#{@currency_two}")
+
+  require "json"
+  parsed_convert_response = JSON.parse(convert_response)
+
+  @result_hash = parsed_convert_response.fetch('result')
+
+  erb(:results)
 end
